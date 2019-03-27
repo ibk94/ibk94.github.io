448351cea91811d83204fa9e32fbdcab26b0c01d
Merlot <csav6823@uibk.ac.at>
2019-03-27 16:53:58 +0100

    nur noch Orte anpassen

--- Adler/main.js
+++ Adler/main.js
@@ -1,14 +1,12 @@
 /*Skript für Adlerweg; */
 
 const div = document.getElementById("map");
-const breite = div.getAttribute("data-lat");
-const laenge = div.getAttribute("data-lng");
-const titel = div.getAttribute("data-title");
-
-const div2 = document.getElementById("map2");
-const breite2 = div.getAttribute("data2-lat");
-const laenge2 = div.getAttribute("data2-lng");
-const titel2 = div.getAttribute("data2-title");
+const breite1 = div.getAttribute("data-lat1");
+const laenge1 = div.getAttribute("data-lng1");
+const titel1 = div.getAttribute("data-title1");
+const breite2 = div.getAttribute("data-lat2");
+const laenge2 = div.getAttribute("data-lng2");
+const titel2 = div.getAttribute("data-title2");
 
 //console.log("Breite=",breite,"Länge=",laenge,"Title=",titel);
 
@@ -18,18 +16,23 @@ let karte = L.map("map");
  
 // auf Ausschnitt zoomen
 karte.setView(
-    [breite,laenge],
-    13
+    [breite1,laenge1],
+    11
 );
 
 // open street map einbauen
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
 //Positionsmarker hinzufügen
-let pin = L.marker(
-    [breite,laenge]
+let pin1 = L.marker(
+    [breite1,laenge1]
 ).addTo(karte);
+pin1.bindPopup(titel1).openPopup();
 
+//Positionsmarker hinzufügen
+let pin2 = L.marker(
+    [breite2,laenge2]
+).addTo(karte);
+pin2.bindPopup(titel2).openPopup();
 //Popup zum Pin hängen
-pin.bindPopup(titel).openPopup();
-pin.bindPopup(titel2).openPopup();
+
