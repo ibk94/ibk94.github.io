78e866eac3810efd19f0d1363eee8d8197a3c907
ibk94 <roman.merlot@student.uibk.ac.at>
2019-04-03 17:00:51 +0200

    Karten hochgeladen

--- adler/main.js
+++ adler/main.js
@@ -56,7 +56,21 @@ const kartenlayer = {
 // subdomains :["a","b","c"],
 // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 //}).addTo(karte);
+//eine Karte muss zumindest in die Karte
+kartenlayer.geolandbasemap.addTo(karte);
+// SChleife zum Öffnen der Karten
+L.control.layers({
+    "Geoland Basemap" : kartenlayer.geolandbasemap,
+    "Geoland Basempa Grau": kartenlayer.bmapgrau,
+    "OpenStreetMap" : kartenlayer.osm,
+    "Geoland Basemap Overlay" : kartenlayer.bmapoverlay,
+    "Basemap High DPI" : kartenlayer.bmaphidpi,
+    "Geoland Basemap Orthofoto" : kartenlayer.bmaporthofoto30cm,
+    "Geoland Basemap Gelände" : kartenlayer.bmapgelaende,
+    "Geoland Basemap Oberfläche" : kartenlayer.bmapoberflaeche,
+}).addTo(karte);
 
+//verschiedene Karten einladen
 //kartenlayer.osm.addTo(karte)
 //kartenlayer.geolandbasemap.addTo(karte);
 //kartenlayer.bmapoverlay.addTo(karte);
@@ -64,7 +78,7 @@ const kartenlayer = {
 //kartenlayer.bmaphidpi.addTo(karte);
 //kartenlayer.bmaporthofoto30cm.addTo(karte);
 //kartenlayer.bmapgelaende.addTo(karte);
-kartenlayer.bmapoberflaeche.addTo(karte);
+//kartenlayer.bmapoberflaeche.addTo(karte);
 
 //Positionsmarker hinzufügen
 let pin1 = L.marker(
