567b6c591980515b96a9b2acd2abe1cf4c54abe8
ibk94 <roman.merlot@student.uibk.ac.at>
2019-05-22 18:46:28 +0200

    Farbe angepasst höhenprofil top, Ende

--- adler/main.js
+++ adler/main.js
@@ -143,21 +143,51 @@ karte.on('click', function (e) {
 
 //gpx datein verwenden können
 new L.GPX("AdlerwegEtappe09.gpx", {
-        async: true,
-        marker_options: {
-            startIconUrl: 'icons/pin-icon-start.png',
-            endIconUrl: 'icons/pin-icon-end.png',
-            shadowUrl: 'icons/pin-shadow.png'
-        }
-    }).on('loaded', function (e) {
-        karte.fitBounds(e.target.getBounds());
-    }).on("addline", function (e) {
-        console.log("linie geladen");
-        const controlElevation = L.control.elevation({
-            detachedView: true,
-            elevationDiv: "#elevation-div",
-        });
-        controlElevation.addTo(karte);
-        controlElevation.addData(e.line);
-    })
-    .addTo(karte);
+    async: true,
+    marker_options: {
+        startIconUrl: 'icons/pin-icon-start.png',
+        endIconUrl: 'icons/pin-icon-end.png',
+        shadowUrl: 'icons/pin-shadow.png'
+    }
+}).on('loaded', function (e) {
+    karte.fitBounds(e.target.getBounds());
+}).on("addline", function (e) {
+    //console.log("linie geladen");
+    const controlElevation = L.control.elevation({
+        detachedView: true,
+        elevationDiv: "#elevation-div",
+    });
+    controlElevation.addTo(karte);
+    controlElevation.addData(e.line);
+    const gpxLinie = e.line.getLatLngs();
+    //console.log(gpxLinie);
+    for (let i = 1; i < gpxLinie.length; i += 1) {
+        //console.log(gpxLinie[i]);
+        let p1 = gpxLinie[i - 1];
+        let p2 = gpxLinie[i];
+        let dist = karte.distance(
+            [p1.lat, p1.lng],
+            [p2.lat, p2.lng]
+        );
+        let delta = (p2.meta.ele - p1.meta.ele);
+        let proz = (dist != 0 ? delta/ dist * 100.0 : 0).toFixed(1);
+        // console.log("Distanz: ", dist, "Höhendiff: ", delta, "Steigung: ", proz);
+        let farbe = 
+            proz >= 10 ? "#d73027" :
+            proz >= 6 ? '#fc8d59' :
+            proz >= 2 ? '#fee08b' :
+            proz >= 0 ? '#ffffbf' :
+            proz >= -6 ? '#d9ef8b' :
+            proz >= -10 ? '#91cf60' :
+                        '#1a9850' ;
+           // ['#d73027','#fc8d59','#fee08b','#ffffbf','#d9ef8b','#91cf60','#1a9850']
+            L.polyline(
+                [
+                    [p1.lat, p1.lng],
+                    [p2.lat, p2.lng],
+                ], {
+                    color : farbe,
+                }
+            ).addTo(karte);
+    }
+});
